efd37093fd91a69ed625e7f3552d6fd269c0e5f0
Bhavinvora10 <bhavinvora7940@gmail.com>
2024-10-21 18:49:01 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
     console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
     console.log(err.name, err.message);
-    process.exit(1);
-    // server.close(() => {
-    // process.exit(1); //0 stands for success & 1 stands for uncaught exception
-    // });
+    server.close(() => {
+        process.exit(1); // 0 stands for success & 1 stands for uncaught exception
+    });
 });
 
 dotenv.config({ path: './config.env' });
@@ -22,8 +21,6 @@ const DB = process.env.DATABASE.replace(
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
         useUnifiedTopology: true,
     })
     .then(() => console.log('DB connection successful!'));
@@ -33,14 +30,12 @@ const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
-// process.setMaxListeners(0)
-
 /* ERRORS OUTSIDE EXPRESS UNHANDLED REJECTIONS */
 process.on('unhandledRejection', (err) => {
     console.log('UNHANDLED REJECTION! 💥 Shutting down...');
     console.log(err.name, err.message);
 
     server.close(() => {
-        process.exit(1); //0 stands for success & 1 stands for uncaught exception
+        process.exit(1); // 0 stands for success & 1 stands for uncaught exception
     });
 });
